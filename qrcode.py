e7c8044e254aae278685e30b4b02897ff72f1740
keerrorbafus <74449075+keerrorbafus@users.noreply.github.com>
2021-03-23 10:29:03 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -11,13 +11,20 @@ except :
     print('Please Wait ..')
     os.system('pip install pyqrcode')
     os.system('pip install pypng')
-if os.path.isdir('export_qrcode')==False:
-    os.system('mkdir export_qrcode')
+    try :
+        print('Give permission for storage..')
+        os.system('termux-setup-storage')
+        print('permission granted ')
+    except :
+        pass
+    
+if os.path.isdir('/sdcard/export_qrcode')==False:
+    os.system('mkdir /sdcard/export_qrcode')
 print('\033[0;30;42m APPLICATION BY KEERRORBAFUS FOR GENERATING QRCODE OF YOUR OWN \033[0;37;40m')
 while True :
     QRstring = input("\nEnter or paste link: ")
     url = pyqrcode.create(QRstring)
-    iname='export_qrcode/'+time.asctime()+'.png'
+    iname='/sdcard/export_qrcode/'+time.asctime()+'.png'
     url.png(iname,scale=8)
     print('  \033[0;33;40mfile_name : '+iname,'\n  qrcode generated sucessfully ! in [export_qrcode named folder]')
     if input('\033[0;37;40mwant to make another qrcode (y/n)')=='y':
